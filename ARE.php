6fcec628d775d0eb01af5ceb920d5262bcb20f3a
miroslava <miroslava.mo9@gmail.com>
2020-12-18 18:05:27 -0600

    primer proyecto plateria

---
+++ ARE.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<head>
+<title>ARETES</title>
+
+
+	
+</head>
+<center>
+<body>
+<body bgcolor="#030607">
+	<font color="white" face="Times New Rman" size="45">
+	
+<marquee bgcolor=#454545 width="700" height="60" ></h1> ARETES</marquee></h1>
+</body>
+<style type="text/css">
+ .boton_personalizado{
+ text-decoration: none;
+padding:2px;
+    font-weight:100;
+    font-size: 20px;
+  color: WHITE;
+   background-color:BLACK;
+    border-radius: 8px;
+    border: 5px solid #CBCCCF;
+  }
+  
+</style>
+
+
+	<style type="text/css">
+	*{margin:0;padding:0;box-sizing:border-box;font-size:0;}  
+nav a{
+      text-decoration:none;
+      color:inherit;
+      font-size:14px;
+      }
+            
+nav li{
+      display:inline-block;
+      width:150px;
+      height:30px;
+      padding:5px 10px;
+      background-color:#000;
+      border:1px solid #000;
+      text-align:center;
+      color:#fff;
+      position:relative;
+}
+            
+nav li:hover{
+      background-color:#fff;
+      color:#000;
+      }
+            
+nav li>ul{
+      display:none;
+      }
+
+nav li:hover>ul{
+      display:block;
+      position:absolute;
+      top:30px;
+      left:0px;
+}
+</style>
+
+
+
+
+
+
+
+
+<center>
+<a class="boton_personalizado" href="plateria paredes.html">INICIO</a>
+<a class="boton_personalizado" href="ARETES.html">ARETES</a>
+<a class="boton_personalizado" href="paint.html">PULSERAS</a>
+<a class="boton_personalizado" href="Electricidad.html">COLLARES</a>	
+<a class="boton_personalizado" href="Carpinteria.html">ANILLOS</a>
+<a class="boton_personalizado" href="HERRERIA.HTML">ARGOLLAS </a> 
+</center>
+
+
+
+<center>
+<font color="white" face="Times New Roman" size="100">
+<td><h2>Productos</h2></td>
+<img src="C:\Users\MIROS\Desktop\PAGINA HTML\imagenes\aretes\1ll00128.jpg" width="300" height="360" " HSPACE="10" VSPACE="10">
+<img src="C:\Users\MIROS\Desktop\PAGINA HTML\imagenes\aretes\1ll00131.jpg" width="300" height="360" " HSPACE="10" VSPACE="10">
+<img src="C:\Users\MIROS\Desktop\PAGINA HTML\imagenes\aretes\1ll00132.jpg" width="300" height="360" " HSPACE="10" VSPACE="10">
+<img src="C:\Users\MIROS\Desktop\PAGINA HTML\imagenes\aretes\1ll00135.jpg" width="300" height="360" " HSPACE="10" VSPACE="10">
+<img src="C:\Users\MIROS\Desktop\PAGINA HTML\imagenes\aretes\1ll00137.jpg" width="300" height="360" " HSPACE="10" VSPACE="10">
+<img src="C:\Users\MIROS\Desktop\PAGINA HTML\imagenes\aretes\1ll00138.jpg" width="300" height="360" " HSPACE="10" VSPACE="10">
+<img src="" width="300" height="360" " HSPACE="10" VSPACE="10">
+<font color="white" face="Times New Roman" size="100">
+<td><h2>Productos</h2></td>
+<img src="img.jpg" width="300" height="360" " HSPACE="10" VSPACE="10">
+<img src="img.jpg" width="300" height="360" " HSPACE="10" VSPACE="10">
+<img src="img.jpg" width="300" height="360" " HSPACE="10" VSPACE="10">
+<img src="img.jpg" width="300" height="360" " HSPACE="10" VSPACE="10">
+<br></br>
+<font color="white" face="Times New Roman" size="100">
+<a class="boton_personalizado" href="file:///C:/Users/MIROS/Desktop/PAGINA%20HTML/plateria%20paredes.html">Menu Principal</a>
+</center>
+</body>
+</html>
+<br></br>
+<center>
+ <marquee width="100%" BEHAVIOR="alternate"> <img src="016-Rugo.jpg"  width="1500" height="300"> </marquee>
+</center>
+</html>
